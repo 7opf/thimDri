4d7c6cce3e8051e9876a1609ccbb6af6bb16b4fc
esoreqMP <eyal.soreq@mpi-mail.mpg.de>
2020-10-09 07:18:37 +0300

    routine update on Fri Oct  9 04:18:37 UTC 2020

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ with open("README.md", "r") as readme_file:
 requirements = ["numpy>=1.19", "pandas>=1.1"]
 
 setup(
-    name="thim-dri",
-    version="0.1.0",
+    name="thimDri",
+    version="0.1.1",
     author="Eyal Soreq",
     author_email="eyalsoreq@gmail.com",
     description="A package to process thim iot datasets",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/esoreq/thim-dri.git",
+    url="https://github.com/esoreq/thimDri.git",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
-        "Programming Language :: Python :: 3.8.5",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
 )
