e3075dc3977a78ed2f603cf493d320c29d5b8ac1
esoreqMP <eyal.soreq@mpi-mail.mpg.de>
2020-10-09 12:12:58 +0300

    routine update on Fri Oct  9 09:12:58 UTC 2020

--- thimDri/pre_process.py
+++ thimDri/pre_process.py
@@ -3,19 +3,32 @@ import pandas as pd
 import time
 import sys
 import zipfile
-import os, glob
+import os
+from pathlib import Path
 from os import path
 import _pickle as cPickle
 import datetime
 import subprocess
 import re
 import csv
-from pathlib import Path
 
 
 
 
 def tidy_obs_domains(_df):
+    '''
+    Separate the large observation Dataframe to domain specific subsets 
+    i.e. location, sleep and physiology
+    
+    INPUT
+    =====
+    _df = Pandas Dataframe parsed from the Observation.csv contained within 
+    the thim zip files 
+
+    RETURNS
+    ======
+    df = a dictionary containing the three dataframes representing the three domains 
+    '''
     df = {}
     df['location'] = _df[_df.location.notnull()]
     df['location'] = df['location'].drop(columns = ["datetimeReceived","provider","valueQuantity",                                                                 "valueUnit","valueDatetimeStart","valueDatetimeEnd"])
@@ -28,6 +41,21 @@ def tidy_obs_domains(_df):
 
 
 def parse_observation(df,zip_file,pickle_file,verbose=False):
+    '''
+    checks if observation file already exists and if not parses the 
+    one contained within the zip_file which is then stored as a pickle locally 
+    
+    INPUT
+    =====
+    df = Either an already preprocessed dictionary or a boolean
+    zip_file = a string directing to a zip file 
+    pickle_file = a string direction to store the processed data
+    verbose = indicating whether to print out steps timming
+    
+    RETURNS
+    ======
+    a dictionary containing observation data frames 
+    ''' 
     date,_ = file_date(zip_file)
     _, name, _ = file_parts(zip_file)
     if isinstance(df, bool) or df['date']<date:
@@ -53,10 +81,24 @@ def parse_observation(df,zip_file,pickle_file,verbose=False):
             print(f"Saving pickle: {time.perf_counter()-tic:0.2f} seconds")
     return df
 
-def load_observation(zip_file,output_path='../Data/pkl/',verbose=True):
+def load_observation(zip_file,output_path='Data/pkl/',verbose=True):
     '''
-    Given a server thim zip file extract and tidy observation file   
-    '''
+    Given an thim zip_file this function compares the date of any 
+    already existing processed data and if it finds none it will parse 
+    the Observation.csv file and store the processed data locally under 
+    the output_path
+        
+    INPUT
+    =====
+    zip_file = a string directing to a thim zip file 
+    output_path = a string direction to local path
+    verbose = indicating whether to print out steps timming
+    
+    RETURNS
+    ======
+    three Pandas Dataframes in the following order 
+    df['location'],df['sleep'],df['physiology']
+    ''' 
     if verbose:tic = time.perf_counter()
     _, name, _ = file_parts(zip_file)
     date,_ = file_date(zip_file)
@@ -71,10 +113,25 @@ def load_observation(zip_file,output_path='../Data/pkl/',verbose=True):
 
 
 def pid_mapping(_df, _zip):
+    '''
+    Given a semi-processed pandas DataFrame _df and a zipfile object 
+    associated with a thim zip file this function will try to use the 
+    # internal Patients.csv file to convert anonymised patient id to internal numeric id 
+        
+    INPUT
+    =====
+    _df = a semi-processed pandas DataFrame containing a subject column
+    _zip = a zipfile object  containing a Patients.csv file
+    
+    RETURNS
+    ======
+    _df = a semi-processed pandas DataFrame containing the old subject 
+    column under project_id and a new remapped subject column
+    ''' 
     try:
         _pid = pd.read_csv(_zip.open('Patients.csv'))
     except:
-        _tmp = zipfile.ZipFile('../Data/tihmdri.zip')
+        _tmp = zipfile.ZipFile('Data/tihmdri.zip')
         _pid = pd.read_csv(_tmp.open('Patients.csv'))
         del _tmp
     _df.subject = pd.Categorical(_df.subject)
@@ -86,7 +143,24 @@ def pid_mapping(_df, _zip):
     return _df
 
 
-def merge_observations(files,output_path='../Data/pkl/',verbose=True):
+def merge_observations(files,output_path='Data/pkl/',verbose=True):
+    '''
+    Given a list of paths to zip files this function will iteratively 
+    pre-process all observation.csv files and merge them to 
+    one unified Dataframe per experimental domains 
+            
+    INPUT
+    =====
+    files = a list of file paths to thim zip files 
+    output_path = a place where already pre-processed data can be 
+    either stored or retrived 
+    verbose = indicating whether to print out steps timming
+    
+    RETURNS
+    ======
+    three Pandas Dataframes in the following order 
+    df['location'],df['sleep'],df['physiology']
+    ''' 
     if verbose:tic = time.perf_counter()
     pickle_file = f"{output_path}merged_Observations.pkl"
     df = load_pickle(pickle_file)
@@ -112,10 +186,23 @@ def merge_observations(files,output_path='../Data/pkl/',verbose=True):
 
 
 
-def load_flags(zip_file,output_path='../Data/pkl/',verbose=True):
+def load_flags(zip_file,output_path='Data/pkl/',verbose=True):
     '''
-    Given a server thim zip file extract and tidy flags 
-    '''
+    Given an thim zip_file this function compares the date of any 
+    already existing processed data and if it finds none it will parse 
+    the Flags.csv file and store the processed data locally under 
+    the output_path
+        
+    INPUT
+    =====
+    zip_file = a string directing to a thim zip file 
+    output_path = a string direction to local path
+    verbose = indicating whether to print out steps timming
+    
+    RETURNS
+    ======
+    a Pandas Dataframe of project specific flags 
+    ''' 
     if verbose:tic = time.perf_counter()
     _, name, _ = file_parts(zip_file)
     date,_ = file_date(zip_file)
@@ -130,6 +217,21 @@ def load_flags(zip_file,output_path='../Data/pkl/',verbose=True):
 
 
 def parse_flags(_df,zip_file,pickle_file,verbose):
+    '''
+    checks if flags file already exists and if not parses the 
+    one contained within the zip_file which is then stored as a pickle locally 
+    
+    INPUT
+    =====
+    df = Either an already preprocessed dictionary or a boolean
+    zip_file = a string directing to a zip file 
+    pickle_file = a string direction to store the processed data
+    verbose = indicating whether to print out steps timming
+    
+    RETURNS
+    ======
+    a dataFrame containing flags events in tabular form
+    ''' 
     date,_ = file_date(zip_file)
     _, name, _ = file_parts(zip_file)
     if isinstance(_df, bool):
@@ -165,7 +267,23 @@ def parse_flags(_df,zip_file,pickle_file,verbose):
     return _df 
 
 
-def merge_flags(files,output_path='../Data/pkl/',verbose=True):
+def merge_flags(files,output_path='Data/pkl/',verbose=True):
+    '''
+    Given a list of paths to zip files this function will iteratively 
+    pre-process all Flags.csv files and merge them to 
+    one unified Dataframe in tabular form
+            
+    INPUT
+    =====
+    files = a list of file paths to thim zip files 
+    output_path = a place where already pre-processed data can be 
+    either stored or retrived 
+    verbose = indicating whether to print out steps timming
+    
+    RETURNS
+    ======
+    a dataFrame containing flags events in tabular form
+    '''     
     if verbose:tic = time.perf_counter()
     pickle_file = f"{output_path}merged_flags.pkl"
     df = load_pickle(pickle_file)
@@ -185,10 +303,24 @@ def merge_flags(files,output_path='../Data/pkl/',verbose=True):
     return df
 
 
-def load_wellbeing(zip_file,output_path='../Data/pkl/',verbose=True):
+def load_wellbeing(zip_file,output_path='Data/pkl/',verbose=True):
     '''
-    Given a server thim zip file extract and tidy daily questions 
-    '''
+    Given an thim zip_file this function compares the date of any 
+    already existing processed data and if it finds none it will parse 
+    the QuestionnaireResponses.csv file and store the processed data 
+    locally under the output_path
+        
+    INPUT
+    =====
+    zip_file = a string directing to a thim zip file 
+    output_path = a string direction to local path
+    verbose = indicating whether to print out steps timming
+    
+    RETURNS
+    ======
+    a dataFrame containing wellbeing events in tabular form
+
+    ''' 
     if verbose:tic = time.perf_counter()
     _, name, _ = file_parts(zip_file)
     date,_ = file_date(zip_file)
@@ -202,7 +334,22 @@ def load_wellbeing(zip_file,output_path='../Data/pkl/',verbose=True):
     return df
 
 
-def parse_wellbeing(_df,zip_file,pickle_file,verbose):
+def parse_wellbeing(_df,zip_file,pickle_file,verbose=True):
+    '''
+    checks if wellbeing file already exists and if not parses the 
+    one contained within the zip_file which is then stored as a pickle locally 
+    
+    INPUT
+    =====
+    _df = Either an already preprocessed dictionary or a boolean
+    zip_file = a string directing to a zip file 
+    pickle_file = a string direction to store the processed data
+    verbose = indicating whether to print out steps timming
+    
+    RETURNS
+    ======
+    a dataFrame containing flags events in tabular form
+    ''' 
     date,_ = file_date(zip_file)
     _, name, _ = file_parts(zip_file)
     if isinstance(_df, bool):
@@ -233,7 +380,23 @@ def parse_wellbeing(_df,zip_file,pickle_file,verbose):
     return _df 
 
 
-def merge_wellbeing(files,output_path='../Data/pkl/',verbose=True):
+def merge_wellbeing(files,output_path='Data/pkl/',verbose=True):
+    '''
+    Given a list of paths to zip files this function will iteratively 
+    pre-process all QuestionnaireResponses.csv files and merge them to 
+    one unified Dataframe in tabular form
+            
+    INPUT
+    =====
+    files = a list of file paths to thim zip files 
+    output_path = a place where already pre-processed data can be 
+    either stored or retrived 
+    verbose = indicating whether to print out steps timming
+    
+    RETURNS
+    ======
+    a dataFrame containing flags events in tabular form
+    '''     
     if verbose:tic = time.perf_counter()
     pickle_file = f"{output_path}merged_well.pkl"
     df = load_pickle(pickle_file)
@@ -253,6 +416,10 @@ def merge_wellbeing(files,output_path='../Data/pkl/',verbose=True):
     return df
 
 def disp(df):
+    '''
+    Used in notebboks to temporarily allow pandas to show all columns and rows 
+    of the dataframe df - use with care!!!
+    '''
     [m,n]= df.shape
     pd.set_option('display.max_rows', m,'display.max_columns',n)
     display(df)
@@ -260,12 +427,19 @@ def disp(df):
     
     
 def tuplizer(x):
+    '''
+    convert cell contents to tupel for the odd cases where a cell in a dataframe
+    contains either an array or a list 
+    '''
     return tuple(x) if isinstance(x, (np.ndarray, list)) else x
 
   
   
 
-def free():
+def free_memory():
+    '''
+    gives info regarding the amount of free memory in linux systems 
+    '''
     # Get process info
     ps = subprocess.Popen(['ps', '-caxm', '-orss,comm'], stdout=subprocess.PIPE).communicate()[0].decode()
     vm = subprocess.Popen(['vm_stat'], stdout=subprocess.PIPE).communicate()[0].decode()
@@ -300,6 +474,9 @@ def free():
     
     
 def ismember(a, b):
+    '''
+    mimic's Matlabs ismemeber function (should be removed in later versions)
+    '''
     bind = {}
     for i, elt in enumerate(b):
         if elt not in bind:
@@ -308,10 +485,16 @@ def ismember(a, b):
   
   
 def nnz(a):
-  return np.count_nonzero(a)
+    '''
+    shortcut to count non zeros 
+    '''
+    return np.count_nonzero(a)
 
 
 def rename_categories(idx,mapping):
+    '''
+    remapping numpy arrays efficiently 
+    '''
     keys = np.array(list(mapping.keys()))
     values = np.array(list(mapping.values()))
     mapping_array = np.zeros(keys.max()+1, dtype=values.dtype)
@@ -319,9 +502,15 @@ def rename_categories(idx,mapping):
     return mapping_array[idx]
 
 def list_to_csv(my_list,file_name,header):
+    '''
+    saving out lists to files quickely
+    '''
     np.savetxt(file_name, my_list, delimiter=",", fmt='%s', header=header)
 
 def file_parts(file):
+    '''
+    splits a string file to path, file_name, file_type
+    '''
     head_tail = os.path.split(file)
     path = head_tail[0]
     file_name, file_type = head_tail[1].split('.')
@@ -329,6 +518,9 @@ def file_parts(file):
 
 
 def find_duplicate(L):
+    '''
+    identifies duplicates in a list and returns their index
+    '''   
     seen,duplicate = set(),set()
     index = np.zeros(len(L), dtype=bool)
     seen_add, duplicate_add = seen.add, duplicate.add
@@ -343,13 +535,19 @@ def find_duplicate(L):
 
 
 def file_date(path_to_file):
+    '''
+    returns a files time stamp as a datetime object
+    '''
     stat = os.stat(path_to_file)
     date = datetime.datetime.fromtimestamp(stat.st_mtime)
     return date, stat
 
 
 def save_pickle(pickle_file, data):
-    if not path.exists(pickle_file):
+    '''
+    Save data as pickle requires full path 
+    '''
+    if not os.path.exists(pickle_file):
             filepath, _, _ = file_parts(pickle_file)
             Path(filepath).mkdir(parents=True, exist_ok=True)
     output_pickle = open(pickle_file, "wb")
@@ -357,7 +555,10 @@ def save_pickle(pickle_file, data):
     output_pickle.close()
 
 def load_pickle(pickle_file):
-    if path.exists(pickle_file) and path.getsize(pickle_file) > 0:
+    '''
+    loads data from a pickle file 
+    '''
+    if os.path.exists(pickle_file) and os.path.getsize(pickle_file) > 0:
         input_pickle = open(pickle_file, 'rb')
         data = cPickle.load(input_pickle)
         input_pickle.close()
@@ -366,4 +567,7 @@ def load_pickle(pickle_file):
 
 
 def get_mapper(value,key):
+    '''
+    creates a mapping dictionary 
+    '''
     return pd.Series(value,index=key).to_dict()
